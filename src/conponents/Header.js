30fc8a8f6efb2b8bc9978c55d0cabf056a0bd2ff
Sohteum <thtwk1123@gmail.com>
2023-09-05 18:39:25 +0900

    .

--- src/conponents/Header.js
+++ src/conponents/Header.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 
 const Header = () => {
+
+    const [search, setSearch] = useState()
+    const [data, setData] = useState(null);
+
+    const onChangeSearch = (e) => {
+        e.preventDefault()
+        setSearch(e.target.value);
+    }
+
+const onClick =(e)=>{
+    e.preventDefault()
+    // fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    // .then((response) => response.json());
+    fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "Test",
+          body: "Testing!",
+          userId: 1,
+        }),
+      })
+        .then((response) => response.json())
+}
+
     return (
-        < >
-           <header>
+        <>
+            <header>
                 <div className='header'>
-                    <img className='logo' src="img/header.png" alt=""/>
+                    <img className='logo' src="img/header.png" alt="" />
                     <h2>Cocktail Lover</h2>
                 </div>
-                <div class="search">
-                    <input type="text"/>
-                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-           </header>
+                <form className="search">
+                    <input value={search} onChange={onChangeSearch} placeholder='Search your cocktail' type="text" />
+                    <button onClick={onClick}><i className="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+            </header>
         </>
     );
 };
